9e5a9188268f99939c727f05dee9fec8c6489b91
Martin Vilcans <martin@librador.com>
2014-04-27 09:16:34 +0200

    Patch Phaser.Polygon.contains

---
+++ app/scripts/polygonfix.js
@@ -0,0 +1,23 @@
+Phaser.Polygon.prototype.contains = function (x, y) {
+
+    var inside = false;
+
+    // use some raycasting to test hits https://github.com/substack/point-in-polygon/blob/master/index.js
+    for (var i = 0, j = this.points.length - 1; i < this.points.length; j = i++)
+    {
+        var xi = this.points[i].x;
+        var yi = this.points[i].y;
+        var xj = this.points[j].x;
+        var yj = this.points[j].y;
+
+        var intersect = ((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+
+        if (intersect)
+        {
+            inside = !inside;
+        }
+    }
+
+    return inside;
+
+}
